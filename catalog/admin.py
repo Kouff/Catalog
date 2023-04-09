69d1364420f6619a97f379f02d17181d3720c6c6
Kouff <thematuhaimmortal@gmail.com>
2023-04-09 15:27:26 +0300

    fix bugs with models and improve the admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Catalog, Product, ProductParam, ProductImage
+
+admin.site.register(Catalog)
+
+
+class ProductParamInline(admin.StackedInline):
+    model = ProductParam
+    extra = 1
+    extra_to_create = 6
+
+    def get_extra(self, request, obj=None, **kwargs):
+        return self.extra_to_create if obj is None else self.extra
+
+
+class ProductImageInline(admin.StackedInline):
+    model = ProductImage
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductImageInline,
+        ProductParamInline,
+    ]
--- catalog/models.py
+++ catalog/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Catalog(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -11,6 +14,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     catalog = models.ForeignKey(Catalog, models.DO_NOTHING, 'products')
+
+    def __str__(self):
+        return self.name
 
 
 class ProductParam(models.Model):
@@ -21,8 +27,7 @@ class ProductParam(models.Model):
 
 
 class ProductImage(models.Model):
-    file = models.ImageField()
+    file = models.ImageField(upload_to='media/products/')
     title = models.CharField(max_length=255)
 
     product = models.ForeignKey(Product, models.DO_NOTHING, 'images')
-
